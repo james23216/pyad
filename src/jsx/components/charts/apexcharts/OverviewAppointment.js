11879b5fc4df9d109e1f2a317802f869c29a3a80
james23216 <jhmun23216@gmail.com>
2022-04-11 22:29:47 -0700

    overview done

---
+++ src/jsx/components/charts/apexcharts/OverviewAppointment.js
@@ -0,0 +1,100 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+const OverviewAppointment = () => {
+   const series = [
+      {
+         name: "Income",
+         data: [420, 550, 850, 220, 650],
+      },
+      {
+         name: "Expenses",
+         data: [170, 850, 101, 90, 250],
+      },
+   ];
+   const options = {
+      chart: {
+         type: "bar",
+         toolbar: {
+            show: false,
+         },
+      },
+      plotOptions: {
+         bar: {
+            horizontal: false,
+            columnWidth: "55%",
+            endingShape: "rounded",
+         },
+      },
+      dataLabels: {
+         enabled: false,
+      },
+
+      legend: {
+         show: true,
+         fontSize: "12px",
+         fontWeight: 300,
+
+         labels: {
+            colors: "white",
+         },
+         position: "bottom",
+         horizontalAlign: "center",
+         markers: {
+            width: 19,
+            height: 19,
+            strokeWidth: 0,
+            radius: 19,
+            strokeColor: "#fff",
+            fillColors: ["#369DC9", "#D45BFF"],
+            offsetX: 0,
+            offsetY: 0,
+         },
+      },
+      yaxis: {
+         labels: {
+            style: {
+               colors: "white",
+               fontSize: "14px",
+               fontFamily: "Poppins",
+               fontWeight: 100,
+            },
+         },
+      },
+      stroke: {
+         show: true,
+         width: 2,
+         colors: ["transparent"],
+      },
+      xaxis: {
+         labels: {
+            style: {
+               colors: "white"
+            },
+         },
+         categories: ["06", "07", "08", "09", "10"],
+      },
+      fill: {
+         colors: ["#369DC9", "#D45BFF"],
+         opacity: 1,
+      },
+      tooltip: {
+         y: {
+            formatter: function (val) {
+               return "$ " + val + " thousands";
+            },
+         },
+      },
+   };
+
+   return (
+      <ReactApexChart
+         options={options}
+         series={series}
+         type="bar"
+         height={350}
+      />
+   );
+};
+
+export default OverviewAppointment;
